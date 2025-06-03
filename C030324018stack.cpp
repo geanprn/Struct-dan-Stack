aba2a3a4f5eeec2f7c3cfca14a2331acfb83e01f
geanprn <geanpranadaniswara@gmail.com>
2025-06-03 19:01:21 +0800

    commit kedua stack

--- C030324018stack.cpp
+++ C030324018stack.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-
+#include <conio.h>
+#include <windows.h>  
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+#define MAX 100
+
+typedef int ItemType;
+
+typedef struct {
+    ItemType data[MAX];
+    int top;
+} Stack;
+
+void Inisialisasi(Stack *S) {
+    S->top = -1;
+}
+
+int Full(Stack *S) {
+    return S->top == MAX - 1;
+}
+
+int Empty(Stack *S) {
+    return S->top == -1;
+}
+
+void Push(Stack *S, ItemType nilai) {
+    if (Full(S)) {
+        cout << "Stack penuh! Tidak bisa menambahkan data.\n";
+    } else {
+        S->top++;
+        S->data[S->top] = nilai;
+        cout << "Data " << nilai << " berhasil dimasukkan.\n";
+    }
 }
